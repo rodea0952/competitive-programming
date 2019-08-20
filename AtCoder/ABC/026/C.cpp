26ab9d4138f9f204d5390d234aac35328edb68ed
rodea <dragondoor0912@yahoo.co.jp>
2019-08-20 23:06:05 +0900

    2019-08-20

--- AtCoder/ABC/026/C.cpp
+++ AtCoder/ABC/026/C.cpp
@@ -1,32 +1,74 @@
-#include<bits/stdc++.h>
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> P;
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
 
-int MOD = 1e9 + 7;
-ll INF = 1e18;
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
 
-vector<vector<int>> boss(30);
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+constexpr double pi = acos(-1);
+constexpr double EPS = 1e-10;
 
-int dfs(int x){
-    if(boss[x].empty()) return 1;
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
 
-    int maxmoney=0, minmoney=MOD;
-    for(auto i : boss[x]){
-        int money=dfs(i);
-        maxmoney=max(maxmoney, money);
-        minmoney=min(minmoney, money);
+int dfs(int cv, vector<vector<int>> &G){
+
+    bool have_child = false;
+    int mi = inf;
+    int ma = 0;
+    for(auto nv : G[cv]){
+        have_child = true;
+        int val = dfs(nv, G);
+        chmin(mi, val);
+        chmax(ma, val);
     }
-    return maxmoney + minmoney + 1;
+
+    if(!have_child) return 1;
+    else return mi + ma + 1;
 }
 
-int main() {
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
     int n; cin>>n;
-    for(int i=2; i<=n; i++) {
+    vector<vector<int>> G(n);
+    for(int i=1; i<n; i++){
         int b; cin>>b;
-        boss[b].push_back(i);
+        b--;
+        G[b].emplace_back(i);
     }
 
-    cout << dfs(1) << endl;
+    cout << dfs(0, G) << endl;
 }

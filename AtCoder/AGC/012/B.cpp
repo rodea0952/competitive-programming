ab96ff66dca2a8ee03b958ed2095d2a44ce3597b
rodea <dragondoor0912@yahoo.co.jp>
2019-09-18 14:10:16 +0900

    2019-09-18

--- AtCoder/AGC/012/B.cpp
+++ AtCoder/AGC/012/B.cpp
@@ -1,3 +1,4 @@
+#pragma GCC optimize("O3")
 #include <iostream>
 #include <iomanip>
 #include <cstdio>
@@ -23,63 +24,71 @@
 #include <fstream>
 #include <functional>
 #include <bitset>
-#define chmin(a, b) ((a)=min((a), (b)))
-#define chmax(a, b) ((a)=max((a), (b)))
-#define fs first
-#define sc second
-#define eb emplace_back
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> P;
-typedef tuple<int, int, int> T;
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
 
-const ll MOD=1e9+7;
-const ll INF=1e18;
-const double pi=acos(-1);
-const double eps=1e-10;
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
 
-int dx[]={1, 0, -1, 0};
-int dy[]={0, -1, 0, 1};
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+constexpr double pi = acos(-1);
+constexpr double EPS = 1e-10;
 
-vector<vector<int>> g(100010);
-vector<vector<int>> memo(100010, vector<int>(20, 0));
-
-void func(int v, int d, int c){
-    if(memo[v][d] != 0) return;
-    
-    memo[v][d] = c;
-
-    if(d == 0) return;
-
-    func(v, d-1, c);
-
-    for(auto nv:g[v]){
-        func(nv, d-1, c);
-    }
-}
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
 
 int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
     int n, m; cin>>n>>m;
+    vector<vector<int>> G(n);
     for(int i=0; i<m; i++){
         int a, b; cin>>a>>b;
         a--, b--;
-        g[a].eb(b);
-        g[b].eb(a);
+        G[a].emplace_back(b);
+        G[b].emplace_back(a);
     }
+
+    vector<vector<int>> memo(n, vector<int>(11, -1));
+    vector<int> ans(n, 0);
 
     int q; cin>>q;
     vector<int> v(q), d(q), c(q);
-    for(int i=0; i<q; i++){
-        cin>>v[i]>>d[i]>>c[i];
-        v[i]--;
-    }
+    for(int i=0; i<q; i++) cin>>v[i]>>d[i]>>c[i];
 
     for(int i=q-1; i>=0; i--){
-        func(v[i], d[i], c[i]);
+        v[i]--;
+        queue<T> que;
+        que.emplace(v[i], -1, d[i]);
+        while(que.size()){
+            int cv, pv, dist;
+            tie(cv, pv, dist) = que.front(); que.pop();
+
+            bool valid = true;
+            for(int i=dist; i<=10; i++){
+                if(memo[cv][i] != -1) valid = false;
+            }
+
+            if(!valid) continue;
+
+            memo[cv][dist] = c[i];
+            if(ans[cv] == 0) ans[cv] = c[i];
+
+            for(auto nv : G[cv]){
+                if(nv == pv) continue;
+
+                que.emplace(nv, cv, dist - 1);
+            }
+        }
     }
 
-    for(int i=0; i<n; i++){
-        cout << memo[i][0] << endl;
-    }
+    for(int i=0; i<n; i++) cout << ans[i] << endl;
+
+    return 0;
 }

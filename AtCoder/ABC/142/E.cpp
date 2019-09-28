545a4b574386492cf0b05b30e4c6215df7b57d10
rodea <dragondoor0912@yahoo.co.jp>
2019-09-28 23:30:57 +0900

    2019-09-28

---
+++ AtCoder/ABC/142/E.cpp
@@ -0,0 +1,76 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+constexpr double pi = acos(-1);
+constexpr double EPS = 1e-10;
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n, m; cin>>n>>m;
+    vector<int> a(m), b(m);
+    vector<vector<int>> G(m);
+    for(int i=0; i<m; i++){
+        cin>>a[i]>>b[i];
+        for(int j=0; j<b[i]; j++){
+            int c; cin>>c;
+            G[i].emplace_back(--c);
+        }
+    }
+
+    vector<ll> dp(1<<n, INF);
+    dp[0] = 0;
+
+    for(int bit=0; bit<(1<<n); bit++){
+        for(int i=0; i<m; i++){
+            int nbit = bit;
+            for(int j=0; j<b[i]; j++){
+                nbit |= (1LL << G[i][j]);
+            }
+            chmin(dp[nbit], dp[bit] + a[i]);
+        }
+    }
+
+    cout << (dp[(1LL<<n)-1] == INF ? -1 : dp[(1LL<<n)-1]) << endl;
+
+    return 0;
+}

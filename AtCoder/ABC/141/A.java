9bf19bd8a94323c2647523605f1d585d92c92452
rodea <dragondoor0912@yahoo.co.jp>
2019-10-14 23:17:43 +0900

    2019-10-14

---
+++ AtCoder/ABC/141/A.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        if(s.equals("Sunny")) System.out.println("Cloudy");
+        else if(s.equals("Cloudy")) System.out.println("Rainy");
+        else System.out.println("Sunny");
+    }
+}

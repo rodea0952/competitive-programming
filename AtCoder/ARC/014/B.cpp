ada88168654cf404779ae9b93198ab798ea5ee3e
rodea <dragondoor0912@yahoo.co.jp>
2019-10-27 18:56:26 +0900

    2019-10-27

---
+++ AtCoder/ARC/014/B.cpp
@@ -0,0 +1,69 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+constexpr double pi = acos(-1);
+constexpr double EPS = 1e-10;
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n; cin>>n;
+    set<string> Used;
+    vector<string> w(n);
+    for(int i=0; i<n; i++) cin>>w[i];
+    char tail = w[0][0];
+
+    string ans = "DRAW";
+    for(int i=0; i<n; i++){
+        if(tail != w[i][0] || Used.find(w[i]) != Used.end()){
+            ans = (i % 2 ? "WIN" : "LOSE");
+            break;
+        }
+        
+        Used.emplace(w[i]);
+        tail = w[i].back();
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}

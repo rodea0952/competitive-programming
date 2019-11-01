682580269ac31816d33176c89597ee9fef13164f
rodea <dragondoor0912@yahoo.co.jp>
2019-11-01 22:25:50 +0900

    2019-11-01

---
+++ AtCoder/ARC/013/B.cpp
@@ -0,0 +1,63 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+constexpr double pi = acos(-1);
+constexpr double EPS = 1e-10;
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int c; cin>>c;
+    int ansmin = 0, ansmid = 0, ansmax = 0;
+    for(int i=0; i<c; i++){
+        vector<int> v(3);
+        for(int j=0; j<3; j++) cin>>v[j];
+        sort(v.begin(), v.end());
+        chmax(ansmin, v[0]);
+        chmax(ansmid, v[1]);
+        chmax(ansmax, v[2]);
+    }
+
+    cout << ansmin * ansmid * ansmax << endl;
+
+    return 0;
+}

e7bac8598b6c6b55ea38dfccdf952f3b9fc8a503
rodea <dragondoor0912@yahoo.co.jp>
2020-03-02 18:32:11 +0900

    2020-03-02

---
+++ AtCoder/ABC/157/C.cpp
@@ -0,0 +1,71 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+constexpr double pi = acos(-1);
+constexpr double EPS = 1e-10;
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n, m; cin>>n>>m;
+    vector<int> s(m), c(m);
+    for(int i=0; i<m; i++) cin>>s[i]>>c[i], s[i]--;
+
+    for(int i=0; i<=999; i++){
+        string t = to_string(i);
+        if(t.size() != n) continue;
+
+        bool valid = true;
+        for(int j=0; j<m; j++){
+            if(t[s[j]] - '0' != c[j]) valid = false;
+        }
+
+        if(valid){
+            cout << i << endl;
+            return 0;
+        }
+    }
+
+    cout << -1 << endl;
+
+    return 0;
+}

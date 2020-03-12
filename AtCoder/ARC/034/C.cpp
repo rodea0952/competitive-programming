43008de2f2db70a708b220b8fdc0f60a01968e6d
rodea <dragondoor0912@yahoo.co.jp>
2020-03-12 16:50:11 +0900

    2020-03-12

---
+++ AtCoder/ARC/034/C.cpp
@@ -0,0 +1,72 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+
+#define all(a) (a).begin(), (a).end()
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int a, b; cin>>a>>b;
+
+    map<int, ll> cnt;
+    for(int i=b+1; i<=a; i++){
+        int n = i;
+        for(int j=2; j*j<=n; j++){
+            while(n % j == 0){
+                cnt[j]++;
+                n /= j;
+            }
+        }
+        if(n > 1) cnt[n]++;
+    }
+
+    ll ans = 1;
+    for(auto i:cnt){
+        ans *= (i.second + 1);
+        ans %= MOD;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
---
+++ AtCoder/ARC/075/C.cpp
@@ -0,0 +1,116 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+
+#define all(a) (a).begin(), (a).end()
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+// Range Sum Query
+class SegmentTree{
+public:
+    int n;
+    vector<ll> node;
+
+    SegmentTree(vector<ll> v) {
+        int sz = v.size();
+        n = 1; while(n < sz) n *= 2;
+        node.resize(2*n-1, 0);
+        for(int i=0; i<sz; i++) node[i+n-1] = v[i];
+        for(int i=n-2; i>=0; i--) node[i] = node[i*2+1] + node[i*2+2];
+    }
+
+    void add(int x, ll val){
+        x += (n - 1);
+        node[x] += val;
+        while(0 < x){
+            x = (x - 1) / 2;
+            node[x] = node[2*x+1] + node[2*x+2];
+        }
+    }
+
+    ll getsum(int a, int b, int k=0, int l=0, int r=-1){
+        if(r < 0) r = n;
+        if(b <= l || r <= a) return 0;
+        if(a <= l && r <= b) return node[k];
+
+        ll vl = getsum(a, b, 2*k+1, l, (l+r)/2);
+        ll vr = getsum(a, b, 2*k+2, (l+r)/2, r);
+
+        return vl + vr;
+    }
+};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    ll n, k; cin>>n>>k;
+    vector<int> a(n);
+    for(int i=0; i<n; i++) cin>>a[i];
+
+    vector<ll> b(n+1, 0);
+    ll asum = 0;
+    set<ll> st;
+    st.emplace(0);
+    for(int i=0; i<n; i++){
+        asum += a[i];
+        b[i+1] = asum - k * (i + 1);
+        st.emplace(b[i+1]);
+    }
+
+    int id = 0;
+    map<ll, ll> b2id;
+    for(auto i:st) b2id[i] = id++;
+
+    for(int i=0; i<=n; i++){
+        b[i] = b2id[b[i]];
+    }
+
+    SegmentTree seg(vector<ll>(n+1, 0));
+    ll ans = 0;
+    for(int i=n; i>=0; i--){
+        ans += seg.getsum(b[i], n + 1);
+        seg.add(b[i], 1);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}

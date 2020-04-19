38d2796fc10bc565e4d73a83aaa20fbe6ea45726
rodea <dragondoor0912@yahoo.co.jp>
2020-04-19 20:46:18 +0900

    2020-04-19

---
+++ AtCoder/Company/code-festival-2015-qualb/C.cpp
@@ -0,0 +1,72 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+
+#define all(a) (a).begin(), (a).end()
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n, m; cin>>n>>m;
+    multiset<int> a;
+    for(int i=0; i<n; i++){
+        int x; cin>>x;
+        a.emplace(x);
+    }
+
+    for(int i=0; i<m; i++){
+        int x; cin>>x;
+        auto itr = a.lower_bound(x);
+        
+        if(itr == a.end()){
+            cout << "NO" << endl;
+            return 0;
+        }
+        else{
+            a.erase(itr);
+        }
+    }
+
+    cout << "YES" << endl;
+
+    return 0;
+}

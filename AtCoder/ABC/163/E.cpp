f6743f79aa00969dc4287d17a26e568417b5f68c
rodea <dragondoor0912@yahoo.co.jp>
2020-04-20 17:43:21 +0900

    2020-04-20

---
+++ AtCoder/ABC/163/E.cpp
@@ -0,0 +1,79 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+
+#define all(a) (a).begin(), (a).end()
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n; cin>>n;
+    vector<P> A(n);
+    for(int i=0; i<n; i++){
+        int a; cin>>a;
+        A[i] = P(a, i);
+    }
+
+    sort(all(A), greater<P>());
+
+    vector<vector<ll>> dp(n+1, vector<ll>(n+1, -INF));
+    dp[0][0] = 0;
+
+    for(int i=0; i<n; i++){
+        ll a, idx;
+        tie(a, idx) = A[i];
+
+        for(int j=0; j<=i; j++){
+            int l = j, r = i - j;
+            if(dp[l][r] == -INF) continue;
+            chmax(dp[l+1][r], dp[l][r] + a * abs(l - idx));
+            chmax(dp[l][r+1], dp[l][r] + a * abs((n - 1 - r) - idx));
+        }
+    }
+
+    ll ans = -INF;
+    for(int i=0; i<=n; i++) chmax(ans, dp[i][n-i]);
+
+    cout << ans << endl;
+
+    return 0;
+}

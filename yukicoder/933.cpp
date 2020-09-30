5bcf06c1c6115b0e3cb08a18f7894a5d3bf4b42a
rodea <dragondoor0912@yahoo.co.jp>
2020-09-30 18:24:04 +0900

    2020-09-30

---
+++ yukicoder/933.cpp
@@ -0,0 +1,70 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+
+#define all(a) (a).begin(), (a).end()
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n; cin>>n;
+    vector<ll> p(n);
+    for(int i=0; i<n; i++) cin>>p[i];
+
+    sort(all(p));
+
+    if(p.front() == 0){
+        cout << 0 << endl;
+        return 0;
+    }
+
+    ll num = 1;
+    for(int i=0; i<n; i++){
+        p[i] %= 9;
+        num *= p[i];
+        num %= 9;
+    }
+
+    cout << (num == 0 ? 9 : num) << endl;
+
+    return 0;
+}

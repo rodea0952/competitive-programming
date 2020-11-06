60a8e44b0a618472d228c1f9eda07266a4cc1350
rodea <dragondoor0912@yahoo.co.jp>
2020-11-06 18:28:24 +0900

    2020-11-06

---
+++ yukicoder/816.cpp
@@ -0,0 +1,80 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+
+#define all(a) (a).begin(), (a).end()
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+vector<ll> divisor(ll n){
+    vector<ll> v;
+    for(ll i=1; i*i<=n; i++){
+        if(n % i == 0){
+            v.emplace_back(i);
+            v.emplace_back(n / i);
+        }
+    }
+
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+
+    return v;
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    ll a, b; cin>>a>>b;
+
+    auto v = divisor(a + b);
+
+    ll ans = -1;
+    for(auto c : v){
+        if(a == c || b == c) continue;
+        if((b + c) % a == 0 && (a + c) % b == 0){
+            ans = c;
+            break;
+        }
+    }
+
+    cout << (ans == -1 ? -1 : ans) << endl;
+
+    return 0;
+}

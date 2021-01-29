1d35088f1603603435faab44cd99bddf653a72c2
rodea <dragondoor0912@yahoo.co.jp>
2021-01-29 15:19:24 +0900

    2021-01-29

---
+++ AtCoder/ABC/178/D.cpp
@@ -0,0 +1,145 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+
+#define all(a) (a).begin(), (a).end()
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+// https://qiita.com/ue_sho/items/1ee5c3e665c72c035880 より
+class mint{
+public:
+    ll x;
+    constexpr mint(long long x = 0) : x((x % MOD + MOD) % MOD) {}
+    constexpr mint operator-() const{
+        return mint(-x);
+    }
+    constexpr mint& operator+=(const mint& a){
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator-=(const mint& a){
+        if ((x += MOD - a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator*=(const mint& a){
+        (x *= a.x) %= MOD;
+        return *this;
+    }
+    constexpr mint operator+(const mint& a) const{
+        mint res(*this);
+        return res += a;
+    }
+    constexpr mint operator-(const mint& a) const{
+        mint res(*this);
+        return res -= a;
+    }
+    constexpr mint operator*(const mint& a) const{
+        mint res(*this);
+        return res *= a;
+    }
+    constexpr mint pow(ll t) const{
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+    // for prime MOD
+    constexpr mint inv() const{
+        return pow(MOD - 2);
+    }
+    constexpr mint& operator/=(const mint& a){
+        return (*this) *= a.inv();
+    }
+    constexpr mint operator/(const mint& a) const{
+        mint res(*this);
+        return res /= a;
+    }
+};
+istream& operator>>(istream& is, mint& a) {return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) {return os << a.x;}
+
+mint modpow(mint a, ll b){
+    if(b == 0) return 1;
+    else if(b % 2 == 0){
+        mint d = modpow(a, b / 2);
+        return d * d;
+    }
+    else{
+        return a * modpow(a, b - 1);
+    }
+}
+
+const int MAX_N = 3010;
+mint fact[MAX_N], finv[MAX_N];
+mint comb(int n, int r){
+    if(n < r || r < 0) return 0;
+    return fact[n] / fact[n - r] / fact[r];
+}
+
+mint perm(int n, int r){
+    if(n < r || r < 0) return 0;
+    return comb(n, r) * fact[r];
+} 
+
+void fact_init(int n){
+    fact[0] = 1;
+    for(int i=1; i<=n; i++){
+        fact[i] = fact[i - 1] * i;
+    }
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int s; cin>>s;
+
+    fact_init(3005);
+
+    mint ans = 0;
+    for(int i=1; i<=700; i++){
+        ans += comb(s - 3 * i + i - 1, i - 1);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
--- algorithm-library/math_02_PermutationCombination.cpp
+++ algorithm-library/math_02_PermutationCombination.cpp
@@ -1,29 +1,18 @@
-ll modpow(ll a, ll b){
-    if(b == 0) return 1;
-    else if(b % 2 == 0){
-        ll d = modpow(a, b / 2) % MOD;
-        return (d * d) % MOD;
-    }
-    else{
-        return (a * modpow(a, b - 1)) % MOD;
-    }
+const int MAX_N = ;
+mint fact[MAX_N], finv[MAX_N];
+mint comb(int n, int r){
+    if(n < r || r < 0) return 0;
+    return fact[n] / fact[n - r] / fact[r];
 }
 
-ll fact[MAX_N], finv[MAX_N];
-ll comb(int n, int r){
+mint perm(int n, int r){
     if(n < r || r < 0) return 0;
-    return fact[n] * finv[n-r] % MOD * finv[r] % MOD;
-}
-
-ll perm(int n, int r){
-    if(n < r || r < 0) return 0;
-    return comb(n, r) * fact[r] % MOD;
+    return comb(n, r) * fact[r];
 } 
 
 void fact_init(int n){
-    fact[0] = finv[0] = 1;
+    fact[0] = 1;
     for(int i=1; i<=n; i++){
-        fact[i] = (fact[i-1] * i) % MOD;
-        finv[i] = modpow(fact[i], MOD - 2);
+        fact[i] = fact[i - 1] * i;
     }
 }

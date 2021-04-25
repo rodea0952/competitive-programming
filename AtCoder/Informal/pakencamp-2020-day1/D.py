92722d477858c59ec2c9974b8eb46eebc814cfe7
rodea <dragondoor0912@yahoo.co.jp>
2021-04-25 23:05:08 +0900

    2021-04-25

---
+++ AtCoder/Informal/pakencamp-2020-day1/D.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+
+# https://perogram.hateblo.jp/entry/2021/01/07/060844
+if k < n:
+    print(k ** 3)
+elif k < 2 * n:
+    print(k ** 3 - 3 * (k - n) ** 3)
+elif k < 3 * n:
+    print(6 * n ** 3 - (3 * n - k) ** 3)
+else:
+    print(6 * n ** 3)

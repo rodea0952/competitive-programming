632662d9f7b5890be6e22cb69fcec081edd89b7c
rodea <dragondoor0912@yahoo.co.jp>
2022-02-05 13:39:26 +0900

    2022-02-05

---
+++ AtCoder/ABC/221/C.cpp
@@ -0,0 +1,65 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 1e9 + 7;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+
+#define all(a) (a).begin(), (a).end()
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    string s; cin >> s;
+    int n = s.size();
+
+    sort(all(s));
+    ll ans = 0;
+    do{
+        for(int i=1; i<n; i++){
+            ll head = stoll(s.substr(0, i));
+            ll tail = stoll(s.substr(i));
+            chmax(ans, head * tail);
+        }
+    }while(next_permutation(all(s)));
+
+    cout << ans << endl;
+
+    return 0;
+}

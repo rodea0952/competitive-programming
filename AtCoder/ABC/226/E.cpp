54880a391a94e19beed82a2c423a863baf2325ff
rodea <dragondoor0912@yahoo.co.jp>
2022-02-20 18:53:31 +0900

    2022-02-20

---
+++ AtCoder/ABC/226/E.cpp
@@ -0,0 +1,188 @@
+#pragma GCC optimize("O3")
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <complex>
+#include <cmath>
+#include <limits>
+#include <cfloat>
+#include <climits>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <fstream>
+#include <functional>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
+template <class T> inline T chmax(T &a, const T b) {return a = (a < b) ? b : a;}
+template <class T> inline T chmin(T &a, const T b) {return a = (a > b) ? b : a;}
+
+constexpr int MOD = 998244353;
+constexpr int inf = 1e9;
+constexpr long long INF = 1e18;
+
+#define all(a) (a).begin(), (a).end()
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+struct UnionFind{
+    vector<int> par;
+    vector<int> rank;
+    vector<int> sz;
+    int uf_size;
+
+    UnionFind(int n){
+        par.resize(n);
+        rank.resize(n);
+        sz.resize(n);
+        uf_size = n;
+        for(int i=0; i<n; i++){
+            par[i] = i;
+            rank[i] = 0;
+            sz[i] = 1;
+        }
+    }
+
+    int find(int x){
+        if(par[x] == x) return x;
+        else return par[x] = find(par[x]);
+    }
+    
+    bool same(int x, int y){
+        return find(x) == find(y);
+    }
+
+    bool unite(int x, int y){
+        x = find(x);
+        y = find(y);
+        if(x == y) return false;
+        if(rank[x] < rank[y]) swap(x, y);
+        if(rank[x] == rank[y]) rank[x]++;
+        uf_size--;
+        par[y] = x;
+        sz[x] = sz[x] + sz[y];
+        return true;
+    }
+
+    int size(int x){
+        return sz[find(x)];
+    }
+};
+
+struct mint{
+    ll x;
+    constexpr mint(long long x = 0) : x((x % MOD + MOD) % MOD) {}
+    constexpr mint operator-() const{
+        return mint(-x);
+    }
+    constexpr mint& operator+=(const mint& a){
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator-=(const mint& a){
+        if ((x += MOD - a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator*=(const mint& a){
+        (x *= a.x) %= MOD;
+        return *this;
+    }
+    constexpr mint operator+(const mint& a) const{
+        mint res(*this);
+        return res += a;
+    }
+    constexpr mint operator-(const mint& a) const{
+        mint res(*this);
+        return res -= a;
+    }
+    constexpr mint operator*(const mint& a) const{
+        mint res(*this);
+        return res *= a;
+    }
+    constexpr mint pow(ll t) const{
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+    // for prime MOD
+    constexpr mint inv() const{
+        return pow(MOD - 2);
+    }
+    constexpr mint& operator/=(const mint& a){
+        return (*this) *= a.inv();
+    }
+    constexpr mint operator/(const mint& a) const{
+        mint res(*this);
+        return res /= a;
+    }
+};
+istream& operator>>(istream& is, mint& a) {return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) {return os << a.x;}
+
+mint modpow(mint a, ll b){
+    if(b == 0) return 1;
+    else if(b % 2 == 0){
+        mint d = modpow(a, b / 2);
+        return d * d;
+    }
+    else{
+        return a * modpow(a, b - 1);
+    }
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n, m; cin >> n >> m;
+    vector<int> u(m), v(m);
+    UnionFind uf(n);
+    for(int i=0; i<m; i++){
+        cin >> u[i] >> v[i], u[i]--, v[i]--;
+        uf.unite(u[i], v[i]);
+    }
+
+    vector<int> edge_cnt(n, 0);
+    for(int i=0; i<m; i++){
+        edge_cnt[uf.find(u[i])]++;
+    }
+
+    mint ans = 1;
+    for(int i=0; i<n; i++){
+        if(edge_cnt[i] != 0){
+            if(edge_cnt[i] != uf.size(i)){
+                cout << 0 << endl;
+                return 0;
+            }
+            ans *= 2;
+        }
+
+        if(uf.size(i) == 1){
+            cout << 0 << endl;
+            return 0;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
